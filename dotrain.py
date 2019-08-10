268308815e473ae2464a3f05f6f644c49107eaa2
DLW3D <787530307@qq.com>
2019-08-10 21:54:06 +0800

    手动分配GPU

--- dotrain.py
+++ dotrain.py
@@ -17,14 +17,27 @@ from sklearn.metrics import confusion_matrix
 
 import picpro
 
+import tensorflow as tf
+import keras.backend.tensorflow_backend as KTF
+
+# 手动分配GPU
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True  # 不全部占满显存, 按需分配
+config.gpu_options.per_process_gpu_memory_fraction = 0.5  # 指定分配50%空间
+sess = tf.Session(config=config)  # 设置session
+KTF.set_session(sess)
+
 batch_size = 256
 num_classes = 7
 epochs = 100
 
 input_shape = (48, 48, 1)
 
-# x0_train, y0_train, x0_val, y0_val = picpro.ReadFile(r'0.csv', input_shape)
-# x1_train, y1_train, x1_val, y1_val = picpro.ReadFile(r'1.csv', input_shape)
+# 2013
+x_train, y_train, x_val, y_val = picpro.ReadFile(r'train.csv', 10, input_shape)
+
+# x0_train, y0_train, x0_val, y0_val = picpro.ReadFile(r'0.csv', shape=input_shape)
+# x1_train, y1_train, x1_val, y1_val = picpro.ReadFile(r'1.csv', shape=input_shape)
 
 
 print('正在处理训练集...')
@@ -36,7 +49,6 @@ print('正在处理训练集...')
 # del x0_train, y0_train, x0_val, y0_val
 # del x1_train, y1_train, x1_val, y1_val
 
-x_train, y_train, x_val, y_val = picpro.ReadFile(r'train.csv', 10, input_shape)
 
 # 打乱训练集
 # index = [i for i in range(len(y_train))]
@@ -70,7 +82,8 @@ tb_config = keras.callbacks.TensorBoard(
     write_images=True,
     histogram_freq=1)
 early_stopping = callbacks.EarlyStopping(patience=20, mode='auto', verbose=1)
-lrate = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=4, verbose=1, mode='auto', epsilon=0.0001, cooldown=0,
+lrate = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=4, verbose=1, mode='auto', epsilon=0.0001,
+                          cooldown=0,
                           min_lr=0.00001)
 checkpoint = ModelCheckpoint(filepath='save.h5', monitor='val_acc', verbose=1, save_best_only=True,
                              save_weights_only=False, mode='auto', period=1)
@@ -78,17 +91,16 @@ earlystopping = EarlyStopping(monitor='val_acc', patience=20, verbose=1, mode='a
 csvlogger = CSVLogger('log_save.csv', append=False)
 cbks = [lrate, earlystopping, tb_config]  # , checkpoint, csvlogger]
 
-
 # 构建模型
 myseed = 10
 drop_rate = 0.25
 model = Sequential()
 model.add(BatchNormalization(input_shape=input_shape))  # 批量规范化层(不用手动规范化数据了)
-model.add(Conv2D(filters=128, kernel_size=(3, 3), padding='same', kernel_initializer='glorot_normal'))   # 卷积
+model.add(Conv2D(filters=128, kernel_size=(3, 3), padding='same', kernel_initializer='glorot_normal'))  # 卷积
 model.add(LeakyReLU())  # 激活
-model.add(BatchNormalization())     # 批量规范化层
-model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))   # 池化
-model.add(Dropout(rate=drop_rate, seed=myseed))     # Dropout
+model.add(BatchNormalization())  # 批量规范化层
+model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))  # 池化
+model.add(Dropout(rate=drop_rate, seed=myseed))  # Dropout
 
 model.add(Conv2D(filters=128, kernel_size=(3, 3), padding='same', kernel_initializer='glorot_normal'))
 model.add(LeakyReLU())
@@ -120,14 +132,13 @@ model.add(LeakyReLU())
 model.add(BatchNormalization())
 model.add(Dropout(rate=drop_rate, seed=myseed))
 
-model.add(Dense(units=7, activation='softmax', kernel_initializer='glorot_normal'))
+model.add(Dense(units=num_classes, activation='softmax', kernel_initializer='glorot_normal'))
 # compile the model 编译模型
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.adamax(),
               metrics=['accuracy'])
 # 显示模型
 model.summary()
-
 
 # 使用Generator进行图像增强(增加数据集大小)
 generate = ImageDataGenerator(rotation_range=30, width_shift_range=0.2, height_shift_range=0.2, zoom_range=[0.8, 1.2],
@@ -173,6 +184,7 @@ def show_train_history(train_history, train_metrics, validation_metrics):
     plt.xlabel('Epoch')
     plt.legend(['train', 'validation'], loc='upper left')
 
+
 # 显示训练过程
 def plot(history):
     plt.figure(figsize=(12, 4))
@@ -202,10 +214,11 @@ def plot_confusion_matrix(cm, classes,
     plt.xlabel('Predicted label')
     plt.show()
 
+
 # 显示混淆矩阵
 def plot_confuse(model, x_val, y_val):
     predictions = model.predict_classes(x_val)
-    truelabel = y_val.argmax(axis=-1)   # 将one-hot转化为label
+    truelabel = y_val.argmax(axis=-1)  # 将one-hot转化为label
     conf_mat = confusion_matrix(y_true=truelabel, y_pred=predictions)
     plt.figure()
     plot_confusion_matrix(conf_mat, [])
@@ -215,16 +228,14 @@ def plot_confuse(model, x_val, y_val):
 def visual(data, num_layer=1):
     # data:array数据
     # layer:第n层的输出
-    data = np.expand_dims(data, axis=0)     # 开头加一维
+    data = np.expand_dims(data, axis=0)  # 开头加一维
     layer = keras.backend.function([model.layers[0].input], [model.layers[num_layer].output])
     f1 = layer([data])[0]
     # picpro.ArrayToImage(f1[0, :, :, 0] * 255).show()
     num = f1.shape[-1]
     plt.figure(figsize=(8, 8))
     for i in range(num):
-        plt.subplot(np.ceil(np.sqrt(num)), np.ceil(np.sqrt(num)), i+1)
+        plt.subplot(np.ceil(np.sqrt(num)), np.ceil(np.sqrt(num)), i + 1)
         plt.imshow(f1[0, :, :, i] * 255, cmap='gray')
         plt.axis('off')
     plt.show()
-
-
