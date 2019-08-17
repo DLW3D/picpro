a4fd65db0f25247c9e56233569fbf3e82466b07a
DLW3D <787530307@qq.com>
2019-08-18 01:05:57 +0800

    rnn 8/17

--- word_embedding.py
+++ word_embedding.py
@@ -93,8 +93,11 @@ from keras.models import Sequential
 from keras.layers import Embedding, Flatten, Dense
 
 model = Sequential()
+# input (?, maxlen)
 model.add(Embedding(max_words, embedding_dim, input_length=maxlen))
+# (?, maxlen, embedding_dim)
 model.add(Flatten())
+# (?, maxlen * embedding_dim)
 model.add(Dense(32, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 model.summary()
