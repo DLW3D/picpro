79967f0ac566cacb6b5480be90644285943b3c25
DLW3D <787530307@qq.com>
2019-08-18 23:18:15 +0800

    8/18

--- test.py
+++ test.py
@@ -1,219 +1,179 @@
-# ***************** 读取数据
-import os
+"""
+生成器（generator）
+首先，创建一个“生成器（generator）”模型，它将一个矢量（从潜在空间 - 在训练期间随机采样）转换为候选图像。
+GAN通常出现的许多问题之一是generator卡在生成的图像上，看起来像噪声。一种可能的解决方案是在鉴别器（discriminator）
+和生成器（generator）上使用dropout。
+"""
+import keras
+from keras import layers
 import numpy as np
 
-# https://s3.amazonaws.com/keras-datasets/jena_climate_2009_2016.csv.zip
-data_dir = r'D:\78753\.keras\data'
-fname = os.path.join(data_dir, 'jena_climate_2009_2016.csv')
+latent_dim = 32
+height = 32
+width = 32
+channels = 3
 
-f = open(fname)
-data = f.read()
-f.close()
+generator_input = keras.Input(shape=(latent_dim,))
 
-lines = data.split('\n')
-header = lines[0].split(',')
-lines = lines[1:]
+# 首先，将输入转换为16x16 128通道的feature map
+x = layers.Dense(128 * 16 * 16)(generator_input)
+x = layers.LeakyReLU()(x)
+x = layers.Reshape((16, 16, 128))(x)
 
-print(header)
-print(len(lines))
+# 然后，添加卷积层
+x = layers.Conv2D(256, 5, padding='same')(x)
+x = layers.LeakyReLU()(x)
 
-float_data = np.zeros((len(lines), len(header) - 1))
-for i, line in enumerate(lines):
-    float_data[i, :] = [float(x) for x in line.split(',')[1:]]
+# 上采样至 32 x 32
+x = layers.Conv2DTranspose(256, 4, strides=2, padding='same')(x)
+x = layers.LeakyReLU()(x)
 
-# ************* 检查数据
-from matplotlib import pyplot as plt
+# 添加更多的卷积层
+x = layers.Conv2D(256, 5, padding='same')(x)
+x = layers.LeakyReLU()(x)
+x = layers.Conv2D(256, 5, padding='same')(x)
+x = layers.LeakyReLU()(x)
 
-# 显示2009-2016温度变化图
-temp = float_data[:, 1]
-plt.plot(range(len(temp)), temp)
-plt.show()
+# 生成一个 32x32 1-channel 的feature map
+x = layers.Conv2D(channels, 7, activation='tanh', padding='same')(x)
+generator = keras.models.Model(generator_input, x)
+generator.summary()
 
-# 前10天温度变化图(十分钟一个记录,144记录/天)
-plt.plot(range(1440), temp[:1440])
-plt.show()
 
-# ************* 数据生成器
 '''
-data:浮点数据的原始数组，我们刚刚在上面的代码片段中将其规范化。
-lookback:我们的输入数据应该返回多少个时间步骤。
-delay:未来我们的目标应该是多少个时间步骤。
-min_index和max_index:指数data数组，该数组分隔要从哪个时间步骤绘制的时间步骤。这对于保存一段数据以进行验证和另一段数据用于测试非常有用。
-shuffle:是洗牌我们的样品，还是按时间顺序提取样品。
-batch_size:每批样品的数量。
-step:按时间步骤对数据进行抽样的时间。我们将设置为6，以便每小时绘制一个数据点。
-
-假设现在是1点，我们要预测2点时的气温，由于当前数据记录的是每隔10分钟时的气象数据，1点到2点
-间隔1小时，对应6个10分钟，这个6对应的就是delay
-
-要训练网络预测温度，就需要将气象数据与温度建立起对应关系，我们可以从1点开始倒推10天，从过去
-10天的气象数据中做抽样后，形成训练数据。由于气象数据是每10分钟记录一次，因此倒推10天就是从
-当前时刻开始回溯1440条数据，这个1440对应的就是lookback
-
-我们无需把全部1440条数据作为训练数据，而是从这些数据中抽样，每隔6条取一条，
-因此有1440/6=240条数据会作为训练数据，这就是代码中的lookback//step
-
-于是我就把1点前10天内的抽样数据作为训练数据，2点是的气温作为数据对应的正确答案，由此
-可以对网络进行训练
+discriminator(鉴别器)
+创建鉴别器模型，它将候选图像（真实的或合成的）作为输入，并将其分为两类：“生成的图像”或“来自训练集的真实图像”。
 '''
-def generator(data, lookback, delay, min_index, max_index,
-              shuffle=False, batch_size=128, step=6):
-    if max_index is None:
-        max_index = len(data) - delay - 1
-    i = min_index + lookback
-    while 1:
-        if shuffle:
-            rows = np.random.randint(
-                min_index + lookback, max_index, size=batch_size)
-        else:
-            if i + batch_size >= max_index:
-                i = min_index + lookback
-            rows = np.arange(i, min(i + batch_size, max_index))
-            i += len(rows)
-        samples = np.zeros((len(rows),
-                           lookback // step,
-                           data.shape[-1]))
-        targets = np.zeros((len(rows),))
-        for j, row in enumerate(rows):
-            indices = range(rows[j] - lookback, rows[j], step)
-            samples[j] = data[indices]
-            targets[j] = data[rows[j] + delay][1]
-        yield samples, targets
+discriminator_input = layers.Input(shape=(height, width, channels))
+x = layers.Conv2D(128, 3)(discriminator_input)
+x = layers.LeakyReLU()(x)
+x = layers.Conv2D(128, 4, strides=2)(x)
+x = layers.LeakyReLU()(x)
+x = layers.Conv2D(128, 4, strides=2)(x)
+x = layers.LeakyReLU()(x)
+x = layers.Conv2D(128, 4, strides=2)(x)
+x = layers.LeakyReLU()(x)
+x = layers.Flatten()(x)
 
+# 重要的技巧（添加一个dropout层）
+x = layers.Dropout(0, 4)(x)
 
-# ************** 调用数据生成器
-# 选用前200,000个时间戳作为培训数据
-# 减去均值除以标准差
-mean = float_data[:200000].mean(axis=0)
-float_data -= mean
-std = float_data[:200000].std(axis=0)
-float_data /= std
+# 分类层
+x = layers.Dense(1, activation='sigmoid')(x)
 
-lookback = 1440   # 观察将追溯到5天前。
-step = 6   # 观测将在每小时一个数据点取样。
-delay = 144   # 目标是未来24小时。
-batch_size = 128
-
-train_gen = generator(float_data,
-                      lookback=lookback,
-                      delay=delay,
-                      min_index=0,
-                      max_index=200000,
-                      shuffle=True,
-                      step=step,
-                      batch_size=batch_size)
-val_gen = generator(float_data,
-                    lookback=lookback,
-                    delay=delay,
-                    min_index=200001,
-                    max_index=300000,
-                    step=step,
-                    batch_size=batch_size)
-test_gen = generator(float_data,
-                     lookback=lookback,
-                     delay=delay,
-                     min_index=300001,
-                     max_index=None,
-                     step=step,
-                     batch_size=batch_size)
-
-# 要遍历整个验证集需要的gen调用次数
-val_steps = (300000 - 200001 - lookback) // batch_size
-test_steps = (len(float_data) - 300001 - lookback) // batch_size
-
-# 常识基线评估(总是预测明天某时的温度等于今天某时的温度的损失)
-def evaluate_naive_method():
-    batch_maes = []
-    for step in range(val_steps):
-        samples, targets = next(val_gen)
-        preds = samples[:, -1, 1]
-        mae = np.mean(np.abs(preds - targets))
-        batch_maes.append(mae)
-    print(np.mean(batch_maes))
-
-evaluate_naive_method()
-# 0.2897359729905486
-
-# **************** 建模
-from keras.models import Sequential
-from keras import layers
-from keras.optimizers import RMSprop
-# **************** 建模(Dense)0.30
-# model = Sequential()
-# model.add(layers.Flatten(input_shape=(lookback // step, float_data.shape[-1])))
-# model.add(layers.Dense(32, activation='relu'))
-# model.add(layers.Dense(1))
-#
-# model.compile(optimizer=RMSprop(), loss='mae')
-# history = model.fit_generator(train_gen,
-#                               steps_per_epoch=500,
-#                               epochs=20,
-#                               validation_data=val_gen,
-#                               validation_steps=val_steps)
-
-# **************** 建模(GRU)0.2640
-# model = Sequential()
-# model.add(layers.GRU(32, input_shape=(None, float_data.shape[-1])))
-# model.add(layers.Dense(1))
-#
-# model.compile(optimizer=RMSprop(), loss='mae')
-# history = model.fit_generator(train_gen,
-#                               steps_per_epoch=500,
-#                               epochs=20,
-#                               validation_data=val_gen,
-#                               validation_steps=val_steps)
-
-# **************** 建模(GRU,DroupOut)0.2619
-# model = Sequential()
-# model.add(layers.GRU(32,
-#                      dropout=0.2,
-#                      recurrent_dropout=0.2,
-#                      input_shape=(None, float_data.shape[-1])))
-# model.add(layers.Dense(1))
-#
-# model.compile(optimizer=RMSprop(), loss='mae')
-# history = model.fit_generator(train_gen,
-#                               steps_per_epoch=500,
-#                               epochs=40,
-#                               validation_data=val_gen,
-#                               validation_steps=val_steps)
-
-# **************** 建模(GRU,DroupOut,Deep)0.2633
-model = Sequential()
-model.add(layers.GRU(32,
-                     dropout=0.1,
-                     recurrent_dropout=0.5,
-                     return_sequences=True,
-                     input_shape=(None, float_data.shape[-1])))
-model.add(layers.GRU(64, activation='relu',
-                     dropout=0.1,
-                     recurrent_dropout=0.5))
-model.add(layers.Dense(1))
-
-model.compile(optimizer=RMSprop(), loss='mae')
-history = model.fit_generator(train_gen,
-                              steps_per_epoch=500,
-                              epochs=40,
-                              validation_data=val_gen,
-                              validation_steps=val_steps)
+discriminator = keras.models.Model(discriminator_input, x)
+discriminator.summary()
 
 
 
+# 为了训练稳定，在优化器中使用学习率衰减和梯度限幅（按值）。
+discriminator_optimizer = keras.optimizers.RMSprop(lr=8e-4, clipvalue=1.0, decay=1e-8)
+discriminator.compile(optimizer=discriminator_optimizer, loss='binary_crossentropy')
 
-# ***************** 显示训练曲线
+
+
+'''
+The adversarial network:对抗网络
+最后，设置GAN，它链接生成器（generator）和鉴别器（discrimitor）。 这是一种模型，经过训练，
+将使生成器（generator）朝着提高其愚弄鉴别器（discrimitor）能力的方向移动。 该模型将潜在的空间点转换为分类决策，
+“假的”或“真实的”，并且意味着使用始终是“这些是真实图像”的标签来训练。 所以训练`gan`将以一种方式更新
+“发生器”的权重，使得“鉴别器”在查看假图像时更可能预测“真实”。 非常重要的是，将鉴别器设置为在训练
+期间被冻结（不可训练）：训练“gan”时其权重不会更新。 如果在此过程中可以更新鉴别器权重，那么将训练鉴别
+器始终预测“真实”。
+'''
+# 将鉴别器（discrimitor）权重设置为不可训练（仅适用于`gan`模型）
+discriminator.trainable = False
+
+gan_input = keras.Input(shape=(latent_dim,))
+gan_output = discriminator(generator(gan_input))
+gan = keras.models.Model(gan_input, gan_output)
+
+gan_optimizer = keras.optimizers.RMSprop(lr=4e-4, clipvalue=1.0, decay=1e-8)
+gan.compile(optimizer=gan_optimizer, loss='binary_crossentropy')
+
+
+
+'''
+  开始训练了。
+  每个epoch：
+   *在潜在空间中绘制随机点（随机噪声）。
+   *使用此随机噪声生成带有“generator”的图像。
+   *将生成的图像与实际图像混合。
+   *使用这些混合图像训练“鉴别器”，使用相应的目标，“真实”（对于真实图像）或“假”（对于生成的图像）。
+   *在潜在空间中绘制新的随机点。
+   *使用这些随机向量训练“gan”，目标都是“这些是真实的图像”。 这将更新发生器的权重（仅因为鉴别器在“gan”内被冻结）
+   以使它们朝向获得鉴别器以预测所生成图像的“这些是真实图像”，即这训练发生器欺骗鉴别器。
+'''
+import os
+from keras.preprocessing import image
+
+# 导入CIFAR10数据集
+(x_train, y_train), (_, _) = keras.datasets.cifar10.load_data()
+
+# 从CIFAR10数据集中选择frog类（class 6）
+x_train = x_train[y_train.flatten() == 6]
+
+# 标准化数据
+x_train = x_train.reshape((x_train.shape[0],) + (height, width, channels)).astype('float32') / 255.
+
+iterations = 10000
+batch_size = 20
+save_dir = '.\\gan_image'
+
+start = 0
+# 开始训练迭代
+for step in range(iterations):
+    # 在潜在空间中抽样随机点
+    random_latent_vectors = np.random.normal(size=(batch_size, latent_dim))
+    # 将随机抽样点解码为假图像
+    generated_images = generator.predict(random_latent_vectors)
+    # 将假图像与真实图像进行比较
+    stop = start + batch_size
+    real_images = x_train[start: stop]
+    combined_images = np.concatenate([generated_images, real_images])
+    # 组装区别真假图像的标签
+    labels = np.concatenate([np.ones((batch_size, 1)),
+                             np.zeros((batch_size, 1))])
+    # 重要的技巧，在标签上添加随机噪声
+    labels += 0.05 * np.random.random(labels.shape)
+    # 训练鉴别器（discrimitor）
+    d_loss = discriminator.train_on_batch(combined_images, labels)
+    # 在潜在空间中采样随机点
+    random_latent_vectors = np.random.normal(size=(batch_size, latent_dim))
+    # 汇集标有“所有真实图像”的标签
+    misleading_targets = np.zeros((batch_size, 1))
+    # 训练生成器（generator）（通过gan模型，鉴别器（discrimitor）权值被冻结）
+    a_loss = gan.train_on_batch(random_latent_vectors, misleading_targets)
+    start += batch_size
+    if start > len(x_train) - batch_size:
+        start = 0
+    if step % 100 == 0:
+        # 保存网络权值
+        gan.save('gan.h5')
+        # 输出metrics
+        print('discriminator loss at step %s: %s' % (step, d_loss))
+        print('adversarial loss at step %s: %s' % (step, a_loss))
+        # 保存生成的图像
+        img = image.array_to_img(generated_images[0] * 255., scale=False)
+        img.save(os.path.join(save_dir, 'generated_frog' + str(step) + '.png'))
+        # 保存真实图像，以便进行比较
+        img = image.array_to_img(real_images[0] * 255., scale=False)
+        img.save(os.path.join(save_dir, 'real_frog' + str(step) + '.png'))
+
+
+# 绘图
 import matplotlib.pyplot as plt
 
-loss = history.history['loss']
-val_loss = history.history['val_loss']
+# 在潜在空间中抽样随机点
+random_latent_vectors = np.random.normal(size=(10, latent_dim))
 
-epochs = range(len(loss))
+# 将随机抽样点解码为假图像
+generated_images = generator.predict(random_latent_vectors)
 
-plt.figure()
-
-plt.plot(epochs, loss, 'bo', label='Training loss')
-plt.plot(epochs, val_loss, 'b', label='Validation loss')
-plt.title('Training and validation loss')
-plt.legend()
+for i in range(generated_images.shape[0]):
+    img = image.array_to_img(generated_images[i] * 255., scale=False)
+    plt.figure()
+    plt.imshow(img)
 
 plt.show()
 
