59b183432f765c12e0d10c1b40cbfcb7c9339fc4
DLW3D <787530307@qq.com>
2019-08-21 12:45:58 +0800

    dcgan 发布

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
+import msvcrt
 import os
 import tensorflow as tf
+from keras import backend as K
 import keras.backend.tensorflow_backend as KTF
 from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator
@@ -14,10 +16,12 @@ config.gpu_options.allow_growth = True  # 不全部占满显存, 按需分配
 sess = tf.Session(config=config)  # 设置session
 KTF.set_session(sess)
 
+# IO参数
 latent_dim = 100
 img_shape = (96, 96, 3)
 
 
+# ************************** 生成器
 def build_generator():
     model = Sequential()
     model.add(layers.Dense(512 * 6 * 6, activation='relu', input_dim=latent_dim))  # 输入维度为100
@@ -43,6 +47,7 @@ def build_generator():
     return models.Model(noise, img)  # 定义一个 一个输入noise一个输出img的模型
 
 
+# ************************** 判别器
 def build_discriminator():
     model = Sequential()
     dropout = 0.4
@@ -80,7 +85,7 @@ def load_dir_img(sorcedir):
     return np.array(data) / 127.5 - 1
 
 
-# ***************************** 建模
+# ************************** 建模
 optimizer = keras.optimizers.Adam(lr=0.0002, beta_1=0.5)
 # 对判别器进行构建和编译
 discriminator = build_discriminator()
@@ -94,50 +99,56 @@ input_noise = models.Input(shape=(latent_dim,))
 combined = models.Model(input_noise, discriminator(generator(input_noise)))
 combined.compile(loss='binary_crossentropy', optimizer=optimizer)
 
-# ************************* Load Data
-epochs = 100
-batch_size = 64
-gdrate = 2  # 训练生成器额外训练倍率
-save_interval = 10
-save_dir = '.\\gan_image'
+# ************************** Load Data
+# 数据来源:https://drive.google.com/drive/folders/1mCsY5LEsgCnc0Txv0rpAUhKVPWVkbw5I?usp=sharing
+x = load_dir_img(r'C:\Users\78753\.keras\data\2faces\96\faces')
 
-data_dir = r'C:\Users\78753\.keras\data\2faces\96\faces'
-x = load_dir_img(data_dir)
-# train_datagen = ImageDataGenerator(rescale=1./255)
-# train_generator = train_datagen.flow_from_directory(
-#     # 目标文件夹
-#     data_dir,
-#     # 规范化图片大小
-#     target_size=img_shape[0:2],
-#     batch_size=batch_size)
 
-# *************************** 训练
-valid = np.ones((batch_size, 1))
-fake = np.zeros((batch_size, 1))
-for epoch in range(epochs):
-    for step in range(x.shape[0] // batch_size):
-        g_loss = -1
-        # 训练判别器
-        imgs = x[step * batch_size:step * batch_size + batch_size]
-        noise = np.random.normal(0, 1, (batch_size, latent_dim))
-        gen_imgs = generator.predict(noise)
-        d_loss_real = discriminator.train_on_batch(imgs, valid)
-        d_loss_fake = discriminator.train_on_batch(gen_imgs, fake)
-        d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
-        # 训练生成器(动态训练比例)
-        for i in range(1+int(gdrate * d_loss[1] * 2)):
+# ************************** 训练
+def run(epochs=10, batch_size=64, gdrate=2, save_interval=10, save_dir='.\\gan_image'):
+    valid = np.ones((batch_size, 1))
+    fake = np.zeros((batch_size, 1))
+    for epoch in range(epochs):
+        for step in range(x.shape[0] // batch_size):
+            # 按q终止
+            while msvcrt.kbhit():
+                char = ord(msvcrt.getch())
+                if char == 113:
+                    return
+            g_loss = -1
+            # 训练判别器
+            imgs = x[step * batch_size:step * batch_size + batch_size]
             noise = np.random.normal(0, 1, (batch_size, latent_dim))
-            g_loss = combined.train_on_batch(noise, valid)
-        # Log
-        if step % save_interval == 0:
-            print("%d:%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, step, d_loss[0], 100 * d_loss[1], g_loss))
-            combined.save('gan.h5')
-            # 保存生成的图像
-            img = image.array_to_img(gen_imgs[0] * 127 + 127., scale=False)
-            img.save(os.path.join(save_dir, 'generated_' + str(epoch) + '_' + str(step) + '.png'))
-            # 保存真实图像，以便进行比较
-            # img = image.array_to_img(imgs[0] * 127 + 127., scale=False)
-            # img.save(os.path.join(save_dir, 'real_' + str(epoch) + '_' + str(step) + '.png'))
+            gen_imgs = generator.predict(noise)
+            d_loss_real = discriminator.train_on_batch(imgs, valid)
+            d_loss_fake = discriminator.train_on_batch(gen_imgs, fake)
+            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+            # 训练生成器(动态训练比例)
+            for i in range(1 + int(gdrate * d_loss[1] * 2)):
+                noise = np.random.normal(0, 1, (batch_size, latent_dim))
+                g_loss = combined.train_on_batch(noise, valid)
+            # Log
+            if step % save_interval == 0:
+                print(
+                    "%d:%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, step, d_loss[0], 100 * d_loss[1], g_loss))
+                combined.save('gan.h5')
+                # 保存生成的图像
+                img = image.array_to_img(gen_imgs[0] * 127 + 127., scale=False)
+                img.save(os.path.join(save_dir, 'generated_' + str(epoch) + '_' + str(step) + '.png'))
+                # 保存真实图像，以便进行比较
+                # img = image.array_to_img(imgs[0] * 127 + 127., scale=False)
+                # img.save(os.path.join(save_dir, 'real_' + str(epoch) + '_' + str(step) + '.png'))
+    combined.save('gan.h5')
 
-combined.save('gan.h5')
 
+# ************************** 生成
+def generate(generator, num=10, save_dir=r'gan_image'):
+    noise = np.random.normal(0, 1, (num, K.int_shape(generator.layers[0].input)[1]))
+    gen_imgs = generator.predict(noise)
+    for i in range(gen_imgs.shape[0]):
+        img = image.array_to_img(gen_imgs[i] * 127 + 127., scale=False)
+        img.save(os.path.join(save_dir, 'generated_' + str(i) + '.png'))
+
+
+# ************************** 运行
+run()
